4b53acc04967264769dfc49e349b870b86d1e1c6
2swap <16504868+2swap@users.noreply.github.com>
2023-06-10 23:04:40 -0700

    composite scenes

--- src/scenes/c4_scene.cpp
+++ src/scenes/c4_scene.cpp
@@ -4,6 +4,10 @@
 #include "Connect4/c4.h"
 using json = nlohmann::json;
 
+inline int C4_RED         = 0xff880000;
+inline int C4_YELLOW      = 0xff888800;
+inline int C4_EMPTY       = 0xff003388;
+
 class C4Scene : public Scene {
 public:
     C4Scene(const json& config, const json& contents);
@@ -18,6 +22,65 @@ private:
     vector<Board> boards;
 };
 
+void draw_c4_disk(Pixels& p, int stonex, int stoney, int col, bool highlight, char annotation){
+    double stonewidth = p.w/16.;
+    int highlightcol = colorlerp(col, 0, .4);
+    int textcol = colorlerp(col, 0, .7);
+    double px = (stonex-WIDTH/2.+.5)*stonewidth+p.w/2;
+    double py = (-stoney+HEIGHT/2.-.5)*stonewidth+p.h/2;
+    if(highlight) p.fill_ellipse(px, py, stonewidth*.48, stonewidth*.48, highlightcol);
+    p.fill_ellipse(px, py, stonewidth*.4, stonewidth*.4, col);
+
+    switch (annotation) {
+        case '+':
+            p.fill_rect(px - stonewidth/4 , py - stonewidth/16, stonewidth/2, stonewidth/8, textcol);  // Draw two rectangles to form a plus sign
+            p.fill_rect(px - stonewidth/16, py - stonewidth/4 , stonewidth/8, stonewidth/2, textcol);
+            break;
+        case '-':
+            p.fill_rect(px - stonewidth/4 , py - stonewidth/16, stonewidth/2, stonewidth/8, textcol);  // Draw a rectangle to form a minus sign
+            break;
+        case '|':
+            p.fill_rect(px - stonewidth/16, py - stonewidth/4 , stonewidth/8, stonewidth/2, textcol);  // Draw a rectangle to form a vertical bar
+            break;
+        case '=':
+            p.fill_rect(px - stonewidth/4 , py - 3*stonewidth/16, stonewidth/2, stonewidth/8, textcol);  // Draw two rectangles to form an equal sign
+            p.fill_rect(px - stonewidth/4 , py + stonewidth/16, stonewidth/2, stonewidth/8, textcol);
+            break;
+        case 't':
+            p.fill_rect(px - stonewidth / 6, py - stonewidth / 8, stonewidth / 3, stonewidth / 12, textcol);  // Draw a rectangle to form a 't'
+            p.fill_rect(px - stonewidth / 24, py - stonewidth / 4, stonewidth / 12, stonewidth / 2, textcol);
+            break;
+        case 'T':
+            p.fill_rect(px - stonewidth / 4, py - stonewidth / 4, stonewidth / 2, stonewidth / 12, textcol);  // Draw a rectangle to form a 'T'
+            p.fill_rect(px - stonewidth / 24, py - stonewidth / 4, stonewidth / 12, stonewidth / 2, textcol);
+            break;
+        case ':':
+            p.fill_ellipse(px, py - stonewidth / 8, stonewidth / 12, stonewidth / 12, textcol);  // Draw an ellipse to form a ':'
+            p.fill_ellipse(px, py + stonewidth / 8, stonewidth / 12, stonewidth / 12, textcol);
+            break;
+        case '0':
+            p.fill_ellipse(px, py, stonewidth / 4, stonewidth / 3, textcol);  // Draw a circle to form a '0'
+            p.fill_ellipse(px, py, stonewidth / 9, stonewidth / 5, col);
+            break;
+        case '.':
+            p.fill_ellipse(px, py, stonewidth / 6, stonewidth / 6, textcol);  // Draw a circle to form a '0'
+            break;
+        default:
+            break;
+    }
+}
+
+void render_c4_board(Pixels& p, Board b){
+    int cols[] = {C4_EMPTY, C4_RED, C4_YELLOW};
+
+    // background
+    p.fill(BLACK);
+    for(int stonex = 0; stonex < WIDTH; stonex++)
+        for(int stoney = 0; stoney < HEIGHT; stoney++)
+            draw_c4_disk(p, stonex, stoney, cols[b.grid[stoney][stonex]], b.highlight[stoney][stonex], b.get_annotation(stonex, stoney));
+
+}
+
 C4Scene::C4Scene(const json& config, const json& contents) : Scene(config, contents) {
     vector<json> boards_json = contents["boards"];
     for (int i = 0; i < boards_json.size(); i++) {
@@ -27,30 +90,30 @@ C4Scene::C4Scene(const json& config, const json& contents) : Scene(config, conte
 }
 
 void C4Scene::render_non_transition(Pixels& p, int which) {
-    p.fill(0);
+    p.fill(BLACK);
     Board b = boards[which];
-    pix.render_c4_board(b);
-    Pixels board_title_pix = eqn_to_pix(latex_text(contents["boards"][which]["name"].get<string>()), 5);
-    pix.copy(board_title_pix, (pix.w - board_title_pix.w)/2, pix.h-200, 1, 1);
+    render_c4_board(pix, b);
+    Pixels board_title_pix = eqn_to_pix(latex_text(contents["boards"][which]["name"].get<string>()), pix.w / 320);
+    pix.copy(board_title_pix, (pix.w - board_title_pix.w)/2, pix.h-pix.w/12, 1, 1);
 }
 
 void C4Scene::render_transition(Pixels& p, int which, double weight) {
-    p.fill(0);
+    p.fill(BLACK);
 
     Board transition = c4lerp(boards[which], (which == boards.size() - 1) ? Board("") : boards[which+1], weight);
-    pix.render_c4_board(transition);
+    render_c4_board(pix, transition);
     
-    string curr_title = "\\text{" + contents["boards"][which]["name"].get<string>() + "}";;
+    string curr_title = "\\text{" + contents["boards"][which]["name"].get<string>() + "}";
     string next_title = (which == contents["boards"].size() - 1)?"\\text{}":"\\text{" + contents["boards"][which+1]["name"].get<string>() + "}";
 
-    Pixels curr_title_pix = eqn_to_pix(curr_title, 5);
-    Pixels next_title_pix = eqn_to_pix(next_title, 5);
+    Pixels curr_title_pix = eqn_to_pix(curr_title, pix.w / 320);
+    Pixels next_title_pix = eqn_to_pix(next_title, pix.w / 320);
     
     if(next_title == curr_title)
-        pix.copy(curr_title_pix, (pix.w - curr_title_pix.w)/2, pix.h-200, 1, 1);
+        pix.copy(curr_title_pix, (pix.w - curr_title_pix.w)/2, pix.h-pix.w/12, 1, 1);
     else{
-        pix.copy(curr_title_pix, (pix.w - curr_title_pix.w)/2, pix.h-200, 1, weight);
-        pix.copy(next_title_pix, (pix.w - next_title_pix.w)/2, pix.h-200, 1, 1-weight);
+        pix.copy(curr_title_pix, (pix.w - curr_title_pix.w)/2, pix.h-pix.w/12, 1, weight);
+        pix.copy(next_title_pix, (pix.w - next_title_pix.w)/2, pix.h-pix.w/12, 1, 1-weight);
     }
 }
 
