f76b60906364c60c407b2dd6984809c2c3a0bc96
2swap <16504868+2swap@users.noreply.github.com>
2023-06-30 03:51:48 -0700

    playin around

--- src/scenes/Mandelbrot/complex.cpp
+++ src/scenes/Mandelbrot/complex.cpp
@@ -1,12 +1,23 @@
 #pragma once
 
 #include <cmath>
+#include <complex>
 
 class Complex {
     public:
         double real, img;
         Complex() : real(0), img(0) {}
         Complex(double r, double i) : real(r), img(i) {}
+        Complex operator +(double d) const {
+            return Complex(real+d, img);
+        }
+        Complex operator -(double d) const {
+            return Complex(real-d, img);
+        }
+        Complex operator *(double d) const {
+            return Complex(d*real, d*img);
+        }
+
         Complex operator +(const Complex &c) const {
             return Complex(real+c.real, img+c.img);
         }
@@ -16,14 +27,43 @@ class Complex {
         Complex operator *(const Complex &c) const {
             return Complex(real*c.real-img*c.img, real*c.img+c.real*img);
         }
-        /*double magnitude(){
+        double magnitude() const {
             return std::hypot(real, img);
-        }*/
+        }
         double magnitude2() const {
             return real*real+img*img;
+        }
+        Complex log() const {
+            return Complex(std::log(magnitude()), atan2(img, real));
+        }
+        Complex exp() const {
+            return Complex(std::exp(real) * cos(img), std::exp(real) * sin(img));
+        }
+        Complex pow(const Complex& exponent) const {
+            /*double a = exponent.real;
+            double b = exponent.img;
+
+            double r = magnitude();
+            double theta = std::atan2(img, real);
+
+            double newR = std::pow(r, a) * std::exp(-b * theta);
+            double newTheta = a * theta + 0.5 * b * std::log(r);
+
+            return Complex(newR * cos(newTheta), newR * sin(newTheta));*/
+            std::complex<double> z(real, img);
+            std::complex<double> x(exponent.real, exponent.img);
+            std::complex<double> a = std::pow(z, x);
+            return Complex(a.real(), a.imag());
         }
         friend ostream& operator<<(std::ostream& os, const Complex& c) {
             os << c.real << " + " << c.img << "i";
             return os;
         }
 };
+
+bool operator==(const Complex& c1, const Complex& c2) {
+    return (c1.real == c2.real) && (c1.img == c2.img);
+}
+bool operator!=(const Complex& c1, const Complex& c2) {
+    return !(c1 == c2);
+}
--- src/scenes/Mandelbrot/mandelbrot.cpp
+++ src/scenes/Mandelbrot/mandelbrot.cpp
@@ -1,5 +1,7 @@
 #pragma once
 
+//#define BURNING_SHIP 1
+
 #include "complex.cpp"
 class Mandelbrot {
     int escape_radius = 2;
@@ -26,6 +28,24 @@ class Mandelbrot {
             return -1;
         }
         int get_depth_complex(Complex z, const Complex& x, const Complex& c, int& depth){//, int& out_coords){
+            if(c == Complex(2, 0)) return get_depth_exp2(z, x, c, depth);
+            int d = 0;
+            double radius2 = 10000;//pow(2, 1/(x.real-1.0));
+            for(d = 0; d < depth; d++){
+                if(z.magnitude2() > radius2) return d;
+                z = z.pow(x) + c;
+#ifdef BURNING_SHIP
+                z.real = std::abs(z.real);
+                z.img = std::abs(z.img);
+#endif
+                d++;
+            }
+            if(z.magnitude2() > radius2){
+                depth++;
+            }
+            return d;
+        }
+        int get_depth_exp2(Complex z, const Complex& x, const Complex& c, int& depth){//, int& out_coords){
             int d = 0;
             for(d = 0; d < depth; d++){
                 if(z.magnitude2() > escape_radius_squared) return d;
