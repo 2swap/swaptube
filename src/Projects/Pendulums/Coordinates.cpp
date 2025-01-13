527d44638231cc4ed9237b67d0fe43fa7e26fd13
2swap <16504868+2swap@users.noreply.github.com>
2025-01-13 11:41:31 +0900

    Prettified coordinates

--- src/Projects/Pendulums/Coordinates.cpp
+++ src/Projects/Pendulums/Coordinates.cpp
@@ -7,10 +7,11 @@ void render_video() {
         {"center_y", "0.5"},
         {"zoom", "10 <microblock_fraction> 2 * ^"},
     });
-    cs.inject_audio_and_render(SilenceSegment(6));
-    cs.state_manager.microblock_transition({
-        {"center_x", "<t> sin <zoom> * 0.5 +"},
-        {"center_y", "<t> cos <zoom> * 0.5 +"},
+    cs.inject_audio_and_render(SilenceSegment(3));
+    cs.state_manager.set({
+        //{"center_x", "<t> sin 1 <zoom> / * 0.5 +"},
+        //{"center_y", "<t> cos 1 <zoom> / * 0.5 +"},
+        {"zoom", "100.01"},
     });
     cs.inject_audio_and_render(SilenceSegment(3));
 }
--- src/Scenes/Common/CoordinateScene.cpp
+++ src/Scenes/Common/CoordinateScene.cpp
@@ -32,46 +32,64 @@ string truncate_tick(double value) {
 class CoordinateScene : public Scene {
 public:
     CoordinateScene(const double width = 1, const double height = 1)
-        : Scene(width, height) {}
+        : Scene(width, height) {
+        state_manager.add_equation("left_x"  , "<center_x> .5 <zoom> / -");
+        state_manager.add_equation("right_x" , "<center_x> .5 <zoom> / +");
+        state_manager.add_equation("top_y"   , "<center_y> .5 <zoom> / -");
+        state_manager.add_equation("bottom_y", "<center_y> .5 <zoom> / +");
+    }
 
     void draw() override {
         render_axes();
     }
 
     void render_axes() {
-        int w = get_width();
-        int h = get_height();
-        double z = state["zoom"];
-        double cx = state["center_x"];
-        double cy = state["center_y"];
-        double sx = cx - .5/z;
-        double sy = cy - .5/z;
-        double ex = cx + .5/z;
-        double ey = cy + .5/z;
-        double log10z = log10(z);
+        const int w = get_width();
+        const int h = get_height();
+        const double z = state["zoom"] + 0.0001;
+        const double rx = state["right_x"];
+        const double ty = state["top_y"];
+        const double lx = state["left_x"];
+        const double by = state["bottom_y"];
+        const double log10z = log10(z);
         int order_mag = floor(-log10z);
-        bool not_fiveish = log10z - floor(log10z) < .5;
+        const double log_decimal = log10z-floor(log10z);
+        bool not_fiveish = log_decimal < .5;
+        const double interpolator = (log_decimal >= .5 ? -1 : 0) + log_decimal * 2;
         for(int d_om = 0; d_om < 2; d_om++){
             double increment = pow(10, order_mag) * (not_fiveish ? 1 : 0.5);
-            double tick_length = (2-d_om) *5; 
-            for(double x = floor(sx/increment)*increment; x < ex; x += increment) {
-                double frac = (x - sx) / (ex - sx);
+            for(double x = floor(lx/increment)*increment; x < rx; x += increment) {
+                double modified_interpolator = interpolator;
+                if(static_cast<int>(round(x / increment)) % 10 == 0) {
+                    if(d_om == 1) continue;
+                    modified_interpolator = 1;
+                }
+                double tick_length = (1+modified_interpolator-d_om) *5; 
+                double number_opacity = d_om == 1 ? (modified_interpolator<.5? 0 : modified_interpolator*2-1) : 1;
+                double frac = (x - lx) / (rx - lx);
                 int x_pix = frac * w;
                 pix.bresenham(x_pix, 0, x_pix, tick_length, OPAQUE_WHITE, 1, 1);
                 ScalingParams sp(40, 20);
-                if(d_om == 0){
+                if(number_opacity > 0){
                     Pixels latex = latex_to_pix(truncate_tick(x), sp);
-                    pix.overlay(latex, x_pix, tick_length);
+                    pix.overlay(latex, x_pix - latex.w/2, tick_length * 1.5, number_opacity);
                 }
             }
-            for(double y = floor(sy/increment)*increment; y < ey; y += increment) {
-                double frac = (y - sy) / (ey - sy);
+            for(double y = floor(ty/increment)*increment; y < by; y += increment) {
+                double modified_interpolator = interpolator;
+                if(static_cast<int>(round(y / increment)) % 10 == 0) {
+                    if(d_om == 1) continue;
+                    modified_interpolator = 1;
+                }
+                double tick_length = (1+modified_interpolator-d_om) *5; 
+                double number_opacity = d_om == 1 ? (modified_interpolator<.5? 0 : modified_interpolator*2-1) : 1;
+                double frac = 1 - (y - ty) / (by - ty);
                 int y_pix = frac * h;
                 pix.bresenham(0, y_pix, tick_length, y_pix, OPAQUE_WHITE, 1, 1);
                 ScalingParams sp(40, 20);
-                if(d_om == 0){
+                if(number_opacity > 0){
                     Pixels latex = latex_to_pix(truncate_tick(y), sp);
-                    pix.overlay(latex, tick_length, y_pix);
+                    pix.overlay(latex, tick_length * 1.5, y_pix - latex.h/2, number_opacity);
                 }
             }
             if(!not_fiveish) order_mag--;
@@ -80,7 +98,7 @@ public:
     }
 
     const StateQuery populate_state_query() const override {
-        return StateQuery{"center_x", "center_y", "zoom"};
+        return StateQuery{"left_x", "right_x", "top_y", "bottom_y", "zoom"};
     }
 
     void mark_data_unchanged() override {}
