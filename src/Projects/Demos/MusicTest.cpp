2f460307da6368a71691507f6624e4c860652499
2swap <16504868+2swap@users.noreply.github.com>
2025-07-17 13:56:03 -0400

    Move MusicDemo to GeneratedBlockDemo and get rid of the MusicScene which isn't really useful (#9)

--- src/Projects/Demos/MusicTest.cpp
+++
@@ -1,8 +0,0 @@
-#include "../Scenes/MusicScene.cpp"
-
-void render_video() {
-    MusicScene ms;
-    vector<float> left; vector<float> right;
-    ms.generate_audio(12, left, right);
-    ms.stage_macroblock_and_render(GeneratedSegment(left, right));
-}
