d7f4d55c4c8ead5cd7fc8332db878bee493f9c76
2swap <16504868+2swap@users.noreply.github.com>
2025-07-17 13:08:42 -0400

    Get rid of obsolete project files

--- src/Projects/LoopAnimationSceneTest.cpp
+++
@@ -1,5 +0,0 @@
-#include "../Scenes/Media/LoopAnimationScene.cpp"
-void render_video(){
-    LoopAnimationScene las1({"f1", "f2", "f3"});
-    las1.stage_macroblock_and_render(SilenceSegment(3));
-}
