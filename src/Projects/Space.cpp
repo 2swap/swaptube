d1e90e031abf91057f450a2cee2096c635465ba3
2swap <16504868+2swap@users.noreply.github.com>
2026-02-28 04:52:45 -0500

    Space video script idea

--- src/Projects/Space.cpp
+++ src/Projects/Space.cpp
@@ -1,5 +1,71 @@
 #include "../Scenes/Math/GeodesicScene.h"
 
+// *space warping all around*
+// *knock knock knock*
+// [Agent] Interdimensional FBI! Open up!
+// [2swap] *softly* Oh crap.
+// [2swap] One moment!
+// (2swap slams kill switch on space warping device, surfaces begin to flatten out)
+// [Agent] Open the door, now!
+// [2swap] Coming, coming!
+// [Agent] *Bursts open the door* Put your hands where I can see them!
+// [Agent] Do you live here?
+// [2swap] Yes officer.
+// [Agent] Our instruments show a large non-zero gaussian curvature in this room. What on earth are you doing in here?
+// [2swap] It wasn't me, I swear!
+// [Agent] Are you in possession of any counterfeit items?
+// [2swap] Like, drugs?
+// [Agent] No! Tesseracts, Klein bottles, Möbius strips...
+// [2swap] Oh, I think I have seen my roommate playing with those before... (points at door)
+// *Agent walks toward door*
+// *2swap turns device back on*
+// *Agent begins to yell, but voice is warped and cut off as space around him is warped and disconnected into a small bubble*
+// [2swap] *muttering* How in the heck did they figure out...
+// *2swap dials a number on his phone while fiddling with the device and warping space to and fro*
+// [Dealer] Hello?
+// [2swap] Dude, you told me this thing was legal!
+// [Dealer] In the USA, no legislation has been passed regarding manipulating the fabric of space.
+// [2swap] Then why is the Interdimensional FBI after me?
+// [Dealer] Oh, yeah, I don't think there's any diplomatic relationship between the US and extradimensional law enforcement. Those guys make up their own laws. It's more like an activist group than anything else, really.
+// [2swap] So, what? What they say doesn't matter?
+// [Dealer] Oh, no, you're probably screwed. Manipulating space is punishable by death in the 4th dimension.
+// [2swap] WHAT?
+// [Dealer] ...
+// [2swap] Why does an interdimensional activist group want me dead for using it!?!
+// [Dealer] It's a party toy that manipulates the fabric of space. They can be used to create some problems...
+// [2swap] What kind of problems?
+// [Dealer] Things like interrupting radio communications by curving the space around the signal, or lensing large amounts of light to a single point and burning holes in things. Oh, and also making black holes and destroying the universe.
+// [2swap] ...
+// [Dealer] See the red button on the side?
+// *2swap presses the red button*
+// [Dealer] Whatever you do, just don't press that one, and you're probably fine. *black hole beginning to form*
+// *2swap repeating 'oh no', spamming kill switch. Black hole shrinks away.*
+// [Dealer] Are you alright?
+// [2swap] *takes a moment to gather composure*
+// [2swap] Let's start from the beginning. How exactly does this thing work?
+// [Dealer] You've heard of space-time, right? Einstein conceived of space and time as being part of the same fabric, and that fabric can be manipulated by mass and energy.
+// [Dealer] In my lab, we found a special kind of exotic matter which divorces the time component from the space components, allowing us to manipulate space alone.
+// [2swap] What exactly do you mean by manipulating space? It, like, warps the things around me?
+// [Dealer] No, it warps space itself. Think of it like this- Alaska and Norway look really far apart on a map. But when we curve that flat map into a globe, they end up close together. Furthermore, the straight line which connects the two is different on the globe than it is on the flat map. By manipulating space, geometry behaves differently.
+// [Dealer] A map is two-dimensional, and by raising it into a three-dimensional space, we can curve it.
+// [Dealer] That device allows you to change the curvature of your room as though you were raising it into the fourth dimension.
+// [2swap] Are you saying my room is entering and leaving the fourth dimension?
+// [Dealer] Well, maybe. As three dimensional beings, it's easy to imagine a curved two-dimensional space by embedding it into our three-dimensional world.
+// [Dealer] This is an extrinsic perspective, since we perceive that curved space as it is embedded in our higher dimension.
+// [Dealer] However, there is an equivalent point of view called the intrinsic perspective, where we can understand the geometry of a curved space from local measurements of angles, distances, and so on, without needing to embed it within a higher dimension at all.
+// [Dealer] For example, when crocheting, one can create a flat sheet by making a regular grid of stitches. By altering the amount of stitches in different areas, we can create a curved surface.
+// [Dealer] But distances in this surface don't depend on its embedding in three-dimensional space: all you need to know is the length of string present between two points.
+// [Dealer] This is the intrinsic perspective. The notion of geometry is intrinsic to a particular space, independent of its representation.
+// [Dealer] In differential geometry, this is known as Gauss's Theorema Egregium.
+// [Dealer] Gauss showed that these two perspectives are mathematically equivalent. The device merely displays the extrinsic perspective to make it easier to visualize the curvature at play.
+// [Dealer] At our lab, we still haven't discovered whether or not there is truly a higher-dimensional embedding being manipulated from a physical perspective, since both interpretations are predictively equivalent.
+// [2swap] Ok... so if I'm understanding you right, the device manipulates the curvature of this space, within which my walls live, but it doesn't actually move the walls themselves. So, why do my walls look like they're moving when I turn it on?
+// [Dealer] Remember how the straight line on the map is different from the straight line on the globe? It's the same idea here. Light itself follows straight lines. So if the geometry of space is warped, the straight line that light follows before reaching your eyes is different, and thus the image of the walls that you see is warped as well.
+// [2swap] How can a straight line not be straight?
+// [Dealer] You're right, it's not really a suitable word. The idea of straightness only really makes sense in a flat geometry, that is, a geometry we call Euclidean. In a non-Euclidean geometry, we call these straight lines geodesics.
+// [Dealer] If you sail the arctic ocean from Alaska to Norway, to you the path seems straight, but on the globe, you are actually curving downwards since the Earth isn't flat. A geodesic is a curve like this- when you're stuck on a curvy space and walk straight without turning left or right, you are following a geodesic.
+// [Dealer] The light inside your room follows a geodesic curve in non-euclidean space.
+
 void render_video() {
     GeodesicScene gs;
 
