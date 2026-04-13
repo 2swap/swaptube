cb2e97ef62635f550a93f8d156ab687880d207cf
2swap <16504868+2swap@users.noreply.github.com>
2026-04-12 20:50:28 -0400

    Done most scenes, GraphScene still needs some refactoring

--- src/DataObjects/HashableString.h
+++ src/DataObjects/HashableString.h
@@ -10,4 +10,5 @@ public:
     void tick(const StateReturn& state);
     bool is_solution() override;
     std::unordered_set<GenericBoard*> get_children() override;
+    void tick(const StateReturn& state);
 };
