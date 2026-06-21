a64cbbe6c1e238614d5d1974f740d0e3c253d641
Balthazar_Maignan <balth.maignan@gmail.com>
2026-06-21 21:30:30 +0200

    Fixed rubiks moves, added support for full alg execution

--- src/DataObjects/Rubiks.cpp
+++ src/DataObjects/Rubiks.cpp
@@ -1,7 +1,8 @@
 
-#include <vector>
-#include <iostream>
-#include <algorithm>
+#include <vector> // for vectors and matrices
+#include <iostream> // to print the cubes in the terminal
+#include <algorithm> // usefull things like reverse for a vector
+#include <sstream> // used in alg tokenisation
 
 
 // Data : cube state, moves to do
@@ -9,10 +10,10 @@
 
 
 // For now, a face is a matrix, and a cube is a vector of 6 faces, maybe in the future, I'll rewrite with either a coordinate
-//system for each piece, or even something like permutations, idk
+//system for each piece, or even something like permutations, this could be usefull for some of the graph views we planned
 
 
-// creates a square matrice, size is "size", with the same char everywhere, I'll use chars to know what color is a sticker.
+// a face is a square matrice, size is "size", with the same char everywhere, each char corresponds to a color.
 class Face{
     public:
         std::vector<std::vector<char>> stickers;
@@ -33,19 +34,20 @@ class Face{
             }
         }
 
-        void rotateClockwise(){
-            int n = stickers.size();
-
-            // 1. transpose
-            for (int i = 0; i < n; i++)
+        void transpose(){
+            for (int i = 0; i < stickers.size(); i++)
             {
-                for (int j = i + 1; j < n; j++)
+                for (int j = i + 1; j < stickers.size(); j++)
                {
                     std::swap(stickers[i][j], stickers[j][i]);
                 }
             }
+        }
 
-            // 2. reverse each row
+        void rotateClockwise(){ // to achieve this, we transpose and then reverse each row
+            transpose();
+
+            int n = stickers.size();
             for (int i = 0; i < n; i++)
             {
                 std::reverse(stickers[i].begin(), stickers[i].end());
@@ -66,7 +68,7 @@ class Cube{
             R(size, 'R'){
         }
 
-        void print(){
+        void print(){ // this could probably be better written using the print function defined in Face class
             int n = U.stickers.size();
 
             // Up face
@@ -117,11 +119,13 @@ class Cube{
             }
         }
 
+        // Here I had to define each face rotation individually, 
+        // this is due to the fact that matrices defining faces have different orientations
         void rotateU(int depth){
             if (depth == 0){
                 U.rotateClockwise();
             }
-            if (depth < U.stickers.size() - 1){ // this does the move on the adjacent faces
+            if (depth < U.stickers.size() - 1){ // this does the move on the adjacent faces, also, -1 because just do D instead
                 std::vector<char> temp = F.stickers[depth];
                 F.stickers[depth] = R.stickers[depth];
                 R.stickers[depth] = B.stickers[depth];
@@ -149,38 +153,21 @@ class Cube{
             if (depth == 0){
                 F.rotateClockwise();
             }
-            if (depth < F.stickers.size() - 1){// -1 because just do B instead of F lol
+            if (depth < F.stickers.size() - 1){
 
-                for (int i = 0; i < n; i++){ // transpose L
-                    for (int j = i + 1; j < n; j++){
-                        std::swap(L.stickers[i][j], L.stickers[j][i]);
-                    }
-                }
-                // TODO make a transpose function for god's sake
-
-                for (int i = 0; i < n; i++){ // transpose R
-                    for (int j = i + 1; j < n; j++){
-                        std::swap(R.stickers[i][j], R.stickers[j][i]);
-                    }
-                }
+                L.transpose();
+                R.transpose();
 
                 std::vector<char> temp = U.stickers[n - 1 - depth];
+                std::reverse(L.stickers[n - 1 - depth].begin(), L.stickers[n - 1 - depth].end());
+                std::reverse(R.stickers[depth].begin(), R.stickers[depth].end());
                 U.stickers[n - 1 - depth] = L.stickers[n - 1 - depth];
                 L.stickers[n - 1 - depth] = D.stickers[depth];
                 D.stickers[depth] = R.stickers[depth];
                 R.stickers[depth] = temp;
 
-                for (int i = 0; i < n; i++){ // transpose L
-                    for (int j = i + 1; j < n; j++){
-                        std::swap(L.stickers[i][j], L.stickers[j][i]);
-                    }
-                }
-
-                for (int i = 0; i < n; i++){ // transpose R
-                    for (int j = i + 1; j < n; j++){
-                        std::swap(R.stickers[i][j], R.stickers[j][i]);
-                    }
-                }
+                L.transpose();
+                R.transpose();
                 
             };
         }
@@ -190,37 +177,21 @@ class Cube{
             if (depth == 0){
                 B.rotateClockwise();
             }
-            if (depth < B.stickers.size() - 1){// -1 because just do B instead of F lol
+            if (depth < B.stickers.size() - 1){
 
-                for (int i = 0; i < n; i++){ // transpose L
-                    for (int j = i + 1; j < n; j++){
-                        std::swap(L.stickers[i][j], L.stickers[j][i]);
-                    }
-                }
-
-                for (int i = 0; i < n; i++){ // transpose R
-                    for (int j = i + 1; j < n; j++){
-                        std::swap(R.stickers[i][j], R.stickers[j][i]);
-                    }
-                }
+                L.transpose();
+                R.transpose();
 
                 std::vector<char> temp = U.stickers[depth];
+                std::reverse(L.stickers[depth].begin(), L.stickers[depth].end());
+                std::reverse(R.stickers[n - 1 - depth].begin(), R.stickers[n - 1 - depth].end());
                 U.stickers[depth] = R.stickers[n - 1 - depth];
                 R.stickers[n - 1 - depth] = D.stickers[n - 1 - depth];
                 D.stickers[n - 1 - depth] = L.stickers[depth];
                 L.stickers[depth] = temp;
 
-                for (int i = 0; i < n; i++){ // transpose L
-                    for (int j = i + 1; j < n; j++){
-                        std::swap(L.stickers[i][j], L.stickers[j][i]);
-                    }
-                }
-
-                for (int i = 0; i < n; i++){ // transpose R
-                    for (int j = i + 1; j < n; j++){
-                        std::swap(R.stickers[i][j], R.stickers[j][i]);
-                    }
-                }
+                L.transpose();
+                R.transpose();
                 
             };
         }
@@ -230,61 +201,25 @@ class Cube{
             if (depth == 0){
                 R.rotateClockwise();
             }
-            if (depth < R.stickers.size() - 1){// -1 because just do B instead of F lol
+            if (depth < R.stickers.size() - 1){
 
-                for (int i = 0; i < n; i++){ // transpose U
-                    for (int j = i + 1; j < n; j++){
-                        std::swap(U.stickers[i][j], U.stickers[j][i]);
-                    }
-                }
-
-                for (int i = 0; i < n; i++){ // transpose F
-                    for (int j = i + 1; j < n; j++){
-                        std::swap(F.stickers[i][j], F.stickers[j][i]);
-                    }
-                }
-
-                for (int i = 0; i < n; i++){ // transpose D
-                    for (int j = i + 1; j < n; j++){
-                        std::swap(D.stickers[i][j], D.stickers[j][i]);
-                    }
-                }
-
-                for (int i = 0; i < n; i++){ // transpose B
-                    for (int j = i + 1; j < n; j++){
-                        std::swap(B.stickers[i][j], B.stickers[j][i]);
-                    }
-                }
+                U.transpose();
+                F.transpose();
+                D.transpose();
+                B.transpose();
 
                 std::vector<char> temp = U.stickers[n - 1 - depth];
+                std::reverse(B.stickers[depth].begin(), B.stickers[depth].end());
+                std::reverse(temp.begin(), temp.end());
                 U.stickers[n - 1 - depth] = F.stickers[n - 1 - depth];
                 F.stickers[n - 1 - depth] = D.stickers[n - 1 - depth];
                 D.stickers[n - 1 - depth] = B.stickers[depth];
                 B.stickers[depth] = temp;
 
-                for (int i = 0; i < n; i++){ // transpose U
-                    for (int j = i + 1; j < n; j++){
-                        std::swap(U.stickers[i][j], U.stickers[j][i]);
-                    }
-                }
-
-                for (int i = 0; i < n; i++){ // transpose F
-                    for (int j = i + 1; j < n; j++){
-                        std::swap(F.stickers[i][j], F.stickers[j][i]);
-                    }
-                }
-
-                for (int i = 0; i < n; i++){ // transpose D
-                    for (int j = i + 1; j < n; j++){
-                        std::swap(D.stickers[i][j], D.stickers[j][i]);
-                    }
-                }
-
-                for (int i = 0; i < n; i++){ // transpose B
-                    for (int j = i + 1; j < n; j++){
-                        std::swap(B.stickers[i][j], B.stickers[j][i]);
-                    }
-                }
+                U.transpose();
+                F.transpose();
+                D.transpose();
+                B.transpose();
                 
             };
         }
@@ -294,71 +229,103 @@ class Cube{
             if (depth == 0){
                 L.rotateClockwise();
             }
-            if (depth < L.stickers.size() - 1){// -1 because just do B instead of F lol
+            if (depth < L.stickers.size() - 1){
 
-                for (int i = 0; i < n; i++){ // transpose U
-                    for (int j = i + 1; j < n; j++){
-                        std::swap(U.stickers[i][j], U.stickers[j][i]);
-                    }
-                }
-
-                for (int i = 0; i < n; i++){ // transpose F
-                    for (int j = i + 1; j < n; j++){
-                        std::swap(F.stickers[i][j], F.stickers[j][i]);
-                    }
-                }
-
-                for (int i = 0; i < n; i++){ // transpose D
-                    for (int j = i + 1; j < n; j++){
-                        std::swap(D.stickers[i][j], D.stickers[j][i]);
-                    }
-                }
-
-                for (int i = 0; i < n; i++){ // transpose B
-                    for (int j = i + 1; j < n; j++){
-                        std::swap(B.stickers[i][j], B.stickers[j][i]);
-                    }
-                }
+                U.transpose();
+                F.transpose();
+                D.transpose();
+                B.transpose();
 
                 std::vector<char> temp = U.stickers[depth];
+                std::reverse(temp.begin(), temp.end());
+                std::reverse(B.stickers[n - 1 - depth].begin(), B.stickers[n - 1 - depth].end());
                 U.stickers[depth] = B.stickers[n - 1 - depth];
                 B.stickers[n - 1 - depth] = D.stickers[depth];
                 D.stickers[depth] = F.stickers[depth];
                 F.stickers[depth] = temp;
 
-                for (int i = 0; i < n; i++){ // transpose U
-                    for (int j = i + 1; j < n; j++){
-                        std::swap(U.stickers[i][j], U.stickers[j][i]);
-                    }
-                }
-
-                for (int i = 0; i < n; i++){ // transpose F
-                    for (int j = i + 1; j < n; j++){
-                        std::swap(F.stickers[i][j], F.stickers[j][i]);
-                    }
-                }
-
-                for (int i = 0; i < n; i++){ // transpose D
-                    for (int j = i + 1; j < n; j++){
-                        std::swap(D.stickers[i][j], D.stickers[j][i]);
-                    }
-                }
-
-                for (int i = 0; i < n; i++){ // transpose B
-                    for (int j = i + 1; j < n; j++){
-                        std::swap(B.stickers[i][j], B.stickers[j][i]);
-                    }
-                }
+                U.transpose();
+                F.transpose();
+                D.transpose();
+                B.transpose();
                 
             };
         }
 
+        struct Move {
+            char face;
+            int depth;
+            int turns;
+        };
+
+        Move parseMove(const std::string& token){
+            Move m;
+
+            size_t pos = 0;
+
+            // depth of the move
+            int layer = 0;
+
+            while (pos < token.size() && std::isdigit(token[pos]))
+            {
+                layer = layer * 10 + (token[pos] - '0'); // dunno why I substracted 0 here
+                pos++;
+            }
+
+            if (layer == 0) {
+                m.depth = 0;
+            } else {
+                m.depth = layer - 1;
+            }
+
+            // face to apply the move
+            m.face = token[pos++];
+
+            // suffixe (number of times we do the move)
+            m.turns = 1;
+            if (pos < token.size())
+            {
+                if (token[pos] == '\'')
+                    m.turns = 3;
+                else if (token[pos] == '2')
+                    m.turns = 2;
+            }
+
+            return m;
+        }
+        
+        // Write alg with the form : dFn, where d is the depth+1, F is the face, and n is either ' or 2, d and n are optionnal.
+        // This does not know moves like M, S, E, r, f etc..., support for those might be added in the future.
+        void exec(const std::string& alg){ 
+            std::stringstream ss(alg);
+            std::string token;
+
+            
+            while (ss >> token) { // separate alg into individual moves (HTM)
+                Move m = parseMove(token);
+
+                for (int i = 0; i < m.turns; ++i) {
+                    switch (m.face) {
+                        case 'U': rotateU(m.depth); break;
+                        case 'D': rotateD(m.depth); break;
+                        case 'F': rotateF(m.depth); break;
+                        case 'B': rotateB(m.depth); break;
+                        case 'R': rotateR(m.depth); break;
+                        case 'L': rotateL(m.depth); break;
+                        default:
+                            std::cerr << "Unknown face : " << m.face << std::endl;
+                            break;
+                    }
+                }
+            }
+        }
+
 };
 
-void rubiks_test(){
-    Cube cube(3);
+int main(){
+    Cube cube(4);
     
-    // change up face stickers
+    // some testing for rotateclockwise
     // cube.B.stickers[0][0] = '1';
     // cube.B.stickers[0][1] = '2';
     // cube.B.stickers[0][2] = '3';
@@ -370,18 +337,16 @@ void rubiks_test(){
     // cube.B.stickers[2][2] = '9';
     
 
-    std::cout << "Before rotation:\n";
+    std::cout << "Before alg:\n";
     cube.print();
 
-    cube.rotateR(0);
-    cube.rotateU(0);
-    cube.rotateR(0);
-    cube.rotateL(0);
+    cube.exec(" R U R' U' R' F R2 U' R' U' R U R' F'"); // yay T-perm
 
-    std::cout << "\n After rotation:\n";
+    std::cout << "\n After alg:\n";
     cube.print();
+    return 0;
 }
 
-// TODO y'a des lignes à inverser par exemple quand on fait L, colonne du fond remplace à l'envers celle du dessus
+
 
 
